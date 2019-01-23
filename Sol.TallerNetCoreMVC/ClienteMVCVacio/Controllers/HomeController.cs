71e22441514cb1f188782d3a2b42c8f7cfd61f33
WILGOP <wilmer-2012xx@hotmail.com>
2019-01-23 00:07:02 -0500

    Commit Inicial

---
+++ Sol.TallerNetCoreMVC/ClienteMVCVacio/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClienteMVCVacio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hola desde MVC";
+        }
+
+        public string Saludar(string nombre)
+        {
+            return "Hola : " + nombre;
+        }
+    }
+}
---
+++ Sol.TallerNetCoreMVC/ClienteMVCVacio/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ClienteMVCVacio
+{
+    public class Startup
+    {
+        public IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //PARA USAR MVC
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //PARA USAR ARCHIVOS ESTATICOS
+            app.UseStaticFiles();
+            //PARA USAR MVC POR DEFECTO
+            app.UseMvcWithDefaultRoute();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World! XDD");
+            });
+        }
+    }
+}
